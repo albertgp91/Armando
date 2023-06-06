7efd10e915257b3567fe215f0fa43bde8de8bba8
samuelesteves <samuelesteves26@hotmail.com>
2023-06-06 12:36:37 +0100

    seeds

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,5 +1,5 @@
 class Letter < ApplicationRecord
   belongs_to :user
-  belongs_to :received, class_name: "User", foreign_key: "received_id"
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
   validates_presence_of :subject
 end
--- db/migrate/20230605153042_add_fields_to_users.rb
+++ db/migrate/20230605153042_add_fields_to_users.rb
@@ -1,7 +1,7 @@
 class AddFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :full_name, :string
-    add_column :users, :received_day, :datetime, default: Date.today + 15
+    add_column :users, :receiving_day, :datetime, default: Date.today + 15
     add_column :users, :user_name, :string
   end
 end
