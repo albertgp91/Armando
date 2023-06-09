b71d9b566fbc54fa07ce0f61120ada951ea29574
albert <albert@cornetbarcelona.com>
2023-06-09 15:48:34 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,42 +12,42 @@ User.destroy_all
 puts "Users deleted"
 
 user1 = User.create!(
-  email: 'user1@example.com',
+  email: 'harry@hogwarts.com',
   password: '123456',
   full_name: 'Harry Potter',
   user_name: 'harry'
 )
 
 user2 = User.create!(
-  email: 'user2@example.com',
+  email: 'peter@neverland.com',
   password: '123456',
-  full_name: 'Justin Bibber',
-  user_name: 'justin'
+  full_name: 'Peter Pan',
+  user_name: 'peter'
 )
 
 
 Letter.create!(
-  content: 'Hi Matilde! It was very nice to meet you in the summer camp!',
-  subject: 'Summer camp',
+  content: "Hi Peter! It was amazing the time that I spent with you in Never Land! I hope I will able to come back very soon, I'm afraid if I take too long I'll be to old for you guys :) Take care!",
+  subject: 'Amazing never land!',
   user_id: user1.id,
   receiver_id: user2.id
 )
 
 Letter.create!(
-  content: 'Hello John! I wanted to ask you if you will go to the event next month?',
-  subject: 'Next event',
+  content: "Dear Harry! I must confess that I have a profound admiration for you. I never saw anyone eating so many cookies. I hope one day I'll be able to do it as well. With respect, Peter",
+  subject: "You're the best!",
   user_id: user2.id,
   receiver_id: user1.id
 )
 Letter.create!(
-  content: 'Hi John! I just wanted to say Merry Christmas!',
-  subject: 'Merry Christmas',
+  content: "Hello Harry! I need to tell you how much I love your magic tricks. I just hope next time you don't make me a developer. All the best!",
+  subject: "You are magic",
   user_id: user2.id,
   receiver_id: user1.id
 )
 Letter.create!(
-  content: 'Hello Matilde! I wanted to ask you how were your holidays?',
-  subject: 'Holidays',
+  content: "Hi Peter, you're so incredibly good playing ping pong. I hope one day you can meet my friend Mateo who will crash you hard in a french way. Keep trainning and good luck!",
+  subject: "Peter Pong vs Mateo",
   user_id: user1.id,
   receiver_id: user2.id
 )
