3a558083e421d5e6c954028f2dbc4ec5c7d0eaa6
albert <albert@cornetbarcelona.com>
2023-06-15 10:45:08 +0100

    another fix attempt

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  config.active_job.queue_adapter = :good_job
   # config.active_job.queue_name_prefix = "Armando_production"
 
   config.action_mailer.perform_caching = false
