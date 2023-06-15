1a41f9cb3d9db8e7f6cd91781343ca56452bc337
albert <albert@cornetbarcelona.com>
2023-06-15 10:43:17 +0100

    another fix attempt

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require_relative "boot"
 require "rails/all"
-require "good_job"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
