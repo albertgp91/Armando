596380e88ccc4929b10d01ad359cb74bcda3cc1f
albert <albert@cornetbarcelona.com>
2023-06-15 10:46:42 +0100

    another fix attempt

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Armando
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    # config.active_job.queue_adapter = :good_job
+    config.active_job.queue_adapter = :good_job
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
